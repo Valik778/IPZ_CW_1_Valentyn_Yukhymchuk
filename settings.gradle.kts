383deaf44ead18e11b8baf4692d82ccedc99eb04
Valik778 <valikuhimcuk7@gmail.com>
2024-04-02 15:30:57 +0300

    1 commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IPZ_CW_1_2 _Yukhymchuk_Valentyn"
+include(":app")
